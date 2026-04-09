799abe3b3a374916b7a21ed2c87877b8f8f1b785
Nathaniel Munk <nathaniel.munk@cloudwave.com.au>
2026-04-09 11:43:28 +1000

    chore: migrate to bun

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export PATH="/opt/homebrew/bin:$PATH"
-eval "$(fnm env --use-on-cd)"
-
 cd "$(dirname "$0")"
-HOST=127.0.0.1 PORT=1337 node build
+HOST=127.0.0.1 PORT=1337 /Users/nmunk/.bun/bin/bun run build/index.js
